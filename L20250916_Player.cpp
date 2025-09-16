01a848b5859789b68544d5c141edc282dad6ed07
saeaj1023-del <saeaj1023@gmail.com>
2025-09-16 14:17:12 +0900

    Update L20250916_Player.cpp

--- L20250916_Player.cpp
+++ L20250916_Player.cpp
@@ -4,41 +4,37 @@
 
 using namespace std;
 
+
 int main()
 {
 	int PlayerX = 0;
 	int PlayerY = 0;
+	bool bIsRunning = true;
 
-	while (true)
-	{
-		int KeyCode = _getch();
+	int ObjectX[100];
+	int ObjectY[100];
+	char ObjectShape[100];
 
-		if (KeyCode == 'w')
-		{
-			PlayerY--;
-		}
-		else if (KeyCode == 's')
-		{
-			PlayerY++;
-		}
-		else if (KeyCode == 'a')
-		{
-			PlayerX--;
-		}
-		else if (KeyCode == 'd')
-		{
-			PlayerX++;
-		}
-		system("cls");
+	int Object01X = 0;
+	int Object01Y = 0;
+	char Object01Shape = '/';
 
-		COORD Cur;
-		Cur.X = PlayerX;
-		Cur.Y = PlayerY;
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+	int Object02X = 0;
+	int Object02Y = 0;
+	char Object02Shape = '?';
 
-		cout << "P" << endl;
-	}
+	int Object03X = 0;
+	int Object03Y = 0;
+	char Object03Shape = '#';
 
+	int Object04X = 0;
+	int Object04Y = 0;
+	char Object04Shape = '%';
 
+	//위치변경
+	cout << Object01Shape << endl;
+	cout << Object02Shape << endl;
+	cout << Object03Shape << endl;
+	cout << Object04Shape << endl;
 	return 0;
 }
